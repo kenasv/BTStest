cfda526a8ce62d8777cc434dca298e1af3362120
kenasv <kenasv@gmail.com>
2017-09-16 17:10:15 +0300

    Оформеление поправил в activity_main.

---
+++ app/src/main/java/com/asuscomm/orangecomp/bts/FirstActivity.java
@@ -0,0 +1,31 @@
+package com.asuscomm.orangecomp.bts;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+import android.content.Intent;
+
+public class FirstActivity extends AppCompatActivity {
+
+    public static final String EXTRA_MESSAGE = "message";
+    //объявляем нашу переменную как константу
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_first);
+
+        Intent intent = getIntent();
+        //запрашиваем данные из интента
+        String messageText = intent.getStringExtra(EXTRA_MESSAGE);
+        //вытаскиваем из интента информацию объекта message
+        TextView messageView = (TextView)findViewById(R.id.message);
+        // присватваем messageView информацию из message
+        messageView.setText(messageText);
+        //выводим информациб message
+    }
+
+
+
+
+
+}
--- app/src/main/java/com/asuscomm/orangecomp/bts/MainActivity.java
+++ app/src/main/java/com/asuscomm/orangecomp/bts/MainActivity.java
@@ -1,14 +1,42 @@
 package com.asuscomm.orangecomp.bts;
+// полное название моего приложения
 
 import android.support.v7.app.AppCompatActivity;
+//основная библиотека на которой строятся приложения на android в Android Studio. Нужна для панели управления
+
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import android.view.View;
+//библиотека для кнопки
 
+import android.content.Intent;
+// библиотека для интента - вызов одной активности из другой
+
+import android.widget.EditText;
+// для использования EditText
+
+public class MainActivity extends AppCompatActivity {
+// наш основной класс Активности, который запускается при запуске при запуске приложения
     @Override
+
     protected void onCreate(Bundle savedInstanceState) {
+        //
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
 
+
+public void button_find (View view){
+    //метод вызываемый при щелчке на кнопку
+
+    EditText messageView = (EditText)findViewById(R.id.message);
+    String messageText = messageView.getText().toString();
+
+    Intent intent = new Intent(this, FirstActivity.class);
+    intent.putExtra(FirstActivity.EXTRA_MESSAGE, messageText);
+    startActivity(intent);
+    //запускаем активность из активности
+
+
+    }
 }

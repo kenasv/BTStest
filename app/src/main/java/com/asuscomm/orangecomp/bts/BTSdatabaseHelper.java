b7aed6a308e7475d2192a933b4001a2ef17d2a5d
kenasv <kenasv@gmail.com>
2017-09-25 01:36:31 +0300

    Создание базы данных v3
    - проверяю создаются ли в базе данных строки

--- app/src/main/java/com/asuscomm/orangecomp/bts/BTSdatabaseHelper.java
+++ app/src/main/java/com/asuscomm/orangecomp/bts/BTSdatabaseHelper.java
@@ -7,6 +7,20 @@ import android.content.ContentValues;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.widget.TextView;
+
+//
+import android.widget.EditText;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+import android.content.Intent;
+import android.widget.Toast;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteException;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.widget.ImageView;
 
 class BTSdatabaseHelper extends SQLiteOpenHelper{
 
@@ -14,20 +28,30 @@ class BTSdatabaseHelper extends SQLiteOpenHelper{
     private static final int DB_VERSION = 1; // Версия базы данных
 
     BTSdatabaseHelper (Context context) {
-        super(context, DB_NAME, null, DB_VERSION)
+        super(context, DB_NAME, null, DB_VERSION);
     }
 
     public void onCreate(SQLiteDatabase db){
-        db.execSQL("CREATE TABLE DRINK (_id INTEGER PRIMARY KEY AUTOINCREMENT, "
+        db.execSQL("CREATE TABLE PL (_id INTEGER PRIMARY KEY AUTOINCREMENT, "
                 + "NOMER TEXT, "
                 + "ADRES TEXT, "
                 + "IMAGE_TIP_ID INTEGER);");
-        insertDrink(db, "33-001", "Студенная", R.drawable.krovla);
-        insertDrink(db, "33-002", "Юбилейная", R.drawable.krovla);
-        insertDrink(db, "33-003", "Каманина", R.drawable.stolb);
-    }
+        insertPL(db, "33-001", "Студенная", R.drawable.krovla);
+        insertPL(db, "33-002", "Юбилейная", R.drawable.krovla);
+        insertPL(db, "33-003", "Каманина", R.drawable.stolb);
 
+    }
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
     }
+        private static void insertPL(SQLiteDatabase db, String nomer,
+                String adres, int imagetipid) {
+            // метод вставки в таблицу данных
+            ContentValues PLValues = new ContentValues();
+            PLValues.put("NOMER", nomer);
+            PLValues.put("ADRES", adres);
+            PLValues.put("IMAGE_TIP_ID INTEGER", imagetipid);
+            db.insert("btsbase", null, PLValues);
+        }
+
 
 }
--- app/src/main/java/com/asuscomm/orangecomp/bts/FirstActivity.java
+++ app/src/main/java/com/asuscomm/orangecomp/bts/FirstActivity.java
@@ -4,12 +4,18 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.TextView;
 import android.content.Intent;
+import android.widget.Toast;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteException;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.widget.ImageView;
+
 
 public class FirstActivity extends AppCompatActivity {
 
-    private BTSdatabaseHelper btsdatabasehelper = new BTSdatabaseHelper();
-    //ссылаемся на класс BTSdatabaseHelper по работе с базой данных
     public static final String EXTRA_MESSAGE = "message";
+
     //объявляем нашу переменную как константу
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,14 +26,57 @@ public class FirstActivity extends AppCompatActivity {
         //запрашиваем данные из интента
         String messageText = intent.getStringExtra(EXTRA_MESSAGE);
         //вытаскиваем из интента информацию объекта message
-        TextView messageView = (TextView)findViewById(R.id.message);
+        TextView messageView = (TextView) findViewById(R.id.message);
         // присватваем messageView информацию из message
         messageView.setText(messageText);
         //выводим информациб message
+
+
+
+
+        TextView popa = (TextView) findViewById(R.id.popa);
+        popa.setText("ggf");
+        //int drinkNo = (Integer)getIntent().getExtras().get(EXTRA_MESSAGE);
+
+        //Создание курсора
+        try {
+            SQLiteOpenHelper btsdatabasehelper = new BTSdatabaseHelper(this);
+
+            TextView nomerz = (TextView) findViewById(R.id.nomer);
+            nomerz.setText("fg");
+
+            SQLiteDatabase db = btsdatabasehelper.getReadableDatabase();
+            Cursor cursor = db.query("PL",
+                    new String[]{"NOMER", "ADRES", "IMAGE_TIP_ID"},
+                    "NOMER = ?",
+                    new String[]{"33-001"},
+                    null, null, null);
+
+            //Переход к первой записи в курсоре
+            if (cursor.moveToFirst()) {
+//Получение данных напитка из курсора
+                String nomerText = cursor.getString(0);
+                String adresText = cursor.getString(1);
+                int photoId = cursor.getInt(2);
+//Заполнение названия напитка
+                TextView nomer = (TextView) findViewById(R.id.nomer);
+                nomer.setText(nomerText);
+//Заполнение описания напитка
+                TextView adres = (TextView) findViewById(R.id.adres);
+                adres.setText(adresText);
+//Заполнение изображения напитка
+                ImageView photo = (ImageView) findViewById(R.id.photo);
+                photo.setImageResource(photoId);
+                photo.setContentDescription(nomerText);
+            }
+            cursor.close();
+            db.close();
+        } catch (SQLiteException e) {
+            Toast toast = Toast.makeText(this, "Database unavailable", Toast.LENGTH_SHORT);
+            toast.show();
+
+
+        }
+
     }
-
-
-
-
-
 }
